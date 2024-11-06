98c22af49ed6eab43bfd09d6e311a25403d37cd8
Shreya-454 <bshreya932@GMAIL.COM>
2024-11-06 14:34:01 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'sea-green':'#00FFE0',
+      },
+      backgroundImage: {
+        'diamond-btn':"url(./assets/images/svg/star.svg)"
+      }
+    },
+  },
+  plugins: [],
+}
+
